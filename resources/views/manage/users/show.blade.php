0a78d5d69096f84ec9498590683eb8ad39bac612
alenhung <alenhung@gmail.com>
2017-11-02 16:12:16 +0800

    修正eitd無法連線問題
    沒指定ID
    修正後：
    {{route('users.edit', $user->id)}}

--- resources/views/manage/users/show.blade.php
+++ resources/views/manage/users/show.blade.php
@@ -7,24 +7,17 @@
         <h1>{{$user->name}}</h1>
       </div>
       <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-        <a href="{{route('users.edit')}}" class="btn">修改會員資料</a>
+        <a href="{{route('users.edit', $user->id)}}" class="btn">修改會員資料</a>
       </div>
       <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
         <div class="panel panel-default">
             <div class="panel-heading">新增管理者相關資料</div>
             <div class="panel-body">
+              <label for="name" class="col-md-4 control-label">使用者名稱</label>
+              <pre>{{$user->name}}</pre>
 
-                    <label for="name" class="col-md-4 control-label">使用者名稱</label>
-                    <pre>{{$user->name}}</pre>
-
-                    <label for="email" class="col-md-4 control-label">電子信箱</label>
-                    <pre>{{$user->email}}</pre>
-
-
-                    <div class="col-md-8 col-md-offset-4">
-                        <button type="submit" class="btn btn-primary">新增管理者</button>
-                    </div>
-
+              <label for="email" class="col-md-4 control-label">電子信箱</label>
+              <pre>{{$user->email}}</pre>
             </div>
         </div>
       </div>
